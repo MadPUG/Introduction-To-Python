fdea878acd63f759c52342a056d9d0d77dbd0c3c
Ian Cordasco <graffatcolmingov@gmail.com>
2014-12-27 13:19:38 -0600

    Add first topic: comments

---
+++ 001_comments.py
@@ -0,0 +1,15 @@
+# All comments in python begin with a '#' also commonly known as a hash-mark,
+# pound sign, and an octothorpe
+
+# Comments are lines in a file that are not executed as instructions by the
+# interpreter. You can write normal language in comments.
+
+# Comments are very important in all kinds of code. They help other
+# programmers and even the person writing the code to understand what they're
+# reading at a later time.
+
+# Not every line of code needs a comment.
+
+# Throughout the rest of the files in this introduction, we, the authors, have
+# left copious amounts of comments so you can refer back to these files as you
+# continue journey into the wonderful world of programming.
